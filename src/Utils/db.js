c7bfa890ec3c4672f8b143a5ae7f6b4812363e73
Ashish-uk <76434546+Ashish-uk@users.noreply.github.com>
2022-05-22 14:52:18 +0530

    cred removed

--- src/Utils/db.js
+++ src/Utils/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const dbParams = {
-    user: 'zspuduhlesrzoq',
-    password: 'a756088dea471751abc1c77f6848ca3c2c917f51140988f30762da874b10536d',
-    host: 'ec2-99-80-170-190.eu-west-1.compute.amazonaws.com',
+    user: '',
+    password: '',
+    host: '',
     port: 5432,
-    database: 'db7npgn9jrlito'
+    database: ''
 }
 
 let dbClient = new Pool({
